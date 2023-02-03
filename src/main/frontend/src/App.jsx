d414e3a5835cee885bda6931b1f812f40bb76c7a
Thadeu José <thadeujs@gmail.com>
2023-02-03 17:12:38 -0300

    front: Show list of employees in front end

--- src/main/frontend/src/App.jsx
+++ src/main/frontend/src/App.jsx
@@ -1,5 +1,15 @@
 import React from "react";
+import { QueryClientProvider, QueryClient } from 'react-query'
+import EmployeesList from "./page/EmployeeList";
 
-export default function App() {
-  return <>Ok4</>;
+export default class App extends React.Component {
+  
+  constructor(){
+    super()
+    this.queryClient = new QueryClient()
+  }  
+
+  render(){
+    return <QueryClientProvider client={this.queryClient}><EmployeesList /></QueryClientProvider>;
+  }
 }
---
+++ src/main/frontend/src/hook/useEmployeesData.jsx
@@ -0,0 +1,12 @@
+import { useQuery } from "react-query";
+import api from "../service/api";
+
+function parseData(data){
+  return data?.data?._embedded?.employees;  // eslint-disable-line no-underscore-dangle
+}
+
+export default function useEmployeesData() {    
+    return useQuery('employees', () => api.get("/employees"), {
+      select: parseData
+    });
+  }
